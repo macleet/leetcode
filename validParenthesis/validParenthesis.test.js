2174bb304047b5150c5e964eea48ab6929eb98ae
Mac Lee <seemin0506@gmail.com>
2024-03-05 16:42:08 -0800

    Add test cases for testing isValid() function.

--- validParenthesis/validParenthesis.test.js
+++ validParenthesis/validParenthesis.test.js
@@ -1,5 +1,41 @@
 'use strict';
 
-function testIsValid() {
-    
+import isValid from './validParenthesis.js';
+
+//  Constraint: 1 <= s.length <= 104
+// const MIN_LEN = 1, MAX_LEN = 104, NUM_PAREN = 6;
+
+function assertIsValid(actual, expected) {
+    if (actual.length !== expected.length || JSON.stringify(actual) !== JSON.stringify(expected)) {
+        console.log('FAIL');
+        console.log('Expected: ', expected);
+        console.log('Actual: ', actual, '\n');
+        return false;
+    } 
+    console.log('PASS');
+    return true;
 }
+
+function testIsValid() {
+    const testcases = [
+        ['()', true],
+        ['()[]{}', true],
+        ['(]', false],
+        ['((()))', true],
+        ['({)}', false],
+        ['())', false],
+        ['][', false],
+        ['(', false],
+        [')', false],
+        ['', true],
+        ['hello', false]
+    ];
+    
+    testcases.forEach(testcase => {
+        const [testStr, expected] = testcase;
+        const actual = isValid(testStr);
+        assertIsValid(actual, expected);
+    });
+}
+
+testIsValid();
