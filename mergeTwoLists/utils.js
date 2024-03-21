e458cb7e9ebf1148628748a19bab09aaca3cc131
Mac Lee <seemin0506@gmail.com>
2024-03-20 17:28:31 -0700

    Move findPivot() function to List class and rename to partition(). Move swapNodes() function to List class and rename to swap().

--- mergeTwoLists/utils.js
+++ mergeTwoLists/utils.js
@@ -13,36 +13,3 @@ export function randList(n, sort = false) {
     randList.append(...randNums);
     return randList;
 }
-
-export function findPivot(head, tail) {
-    let pivot = head;
-    let fast = head;
-    while (fast && fast.next) {
-        pivot = pivot.next;
-        fast = fast.next.next;
-    }
-    const pivots = [head, tail, pivot].sort((a, b) => { return a.val-b.val });
-
-    switch(pivots[1]) {
-        case tail: break;
-        case head:
-            head = pivot.next;
-            tail.next = pivot;
-            head.prev = null;
-            pivot.next = null;
-            break;
-        default:
-            pivot.prev.next = pivot.next;
-            pivot.next.prev = pivot.prev;
-            tail.next = pivot;
-            pivot.next = null;
-            pivot.prev = tail;
-            tail = pivot;
-    }
-
-    return pivots[1];  // return median of three pivots
-}
-
-export function swapNodes() {
-
-}
